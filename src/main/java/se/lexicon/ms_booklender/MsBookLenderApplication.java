54424277498b7452f31daaa0540ae170ce7b7bfc
MariusAdrianStoica <stoichitza@yahoo.com>
2023-01-31 11:07:51 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/ms_booklender/MsBookLenderApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.ms_booklender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBookLenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBookLenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/ms_booklender/MsBookLenderApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.ms_booklender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBookLenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
